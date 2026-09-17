be0feb783484eeffc78505fb6464a631ecee8c58
Augusto Cruz <137422882+augustocruzfisioterapia@users.noreply.github.com>
2026-09-17 08:30:29 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    Instituto Reverte | Augusto Cruz
    ========================================================== */
 
-const CACHE_VERSION = "v1.0.1";
+const CACHE_VERSION = "v1.0.2";
 const CACHE_NAME = `uti-b-indicadores-${CACHE_VERSION}`;
 
 /*
